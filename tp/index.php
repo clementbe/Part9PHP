5c7a5069914730e9823226a811760cb7bf121966
Clementb <clement.benoit5@hotmail.com>
2017-05-09 15:27:19 +0200

    correction tp

--- tp/index.php
+++ tp/index.php
@@ -73,76 +73,82 @@
                 </select>
                 <input type="submit" value="validation">
             </form>
-                 <?php
+            <?php
 
-                 function build_calendar($month, $year) {
-                     // Tableau du jour de la semaine.
-                     $daysOfWeek = array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
-                     //Quel est le premier jour du mois en question?
-                     $firstDayOfMonth = mktime(0, 0, 0, $month, 1, $year);
-                     // Combien de jours contient ce mois-ci?
-                     $numberDays = date('t', $firstDayOfMonth);
-                     // Récupérer des informations sur le premier jour de la
-                     // mois en question.
-                     $dateComponents = getdate($firstDayOfMonth);
-                     // Quel est le nom du mois en question?
-                     $monthName = $dateComponents['month'];
-                     // Quelle est la valeur de l'indice (0-6) du premier jour de la
-                     // mois en question.
-                     $dayOfWeek = $dateComponents['wday'];
-                     // Crée l'ouvre-étiquette de table et les en-têtes de jour
-                     $calendar = "<table class='calendar'>";
-                     $calendar .= "<caption>$monthName $year</caption>";
-                     $calendar .= "<tr>";
-                     // Crée les en-têtes de calendrier
-                     foreach ($daysOfWeek as $day) {
-                         $calendar .= "<th class='header'>$day</th>";
-                     }
-                     // Crée le reste du calendrier
-                     // Lancer le compteur du jour, en commençant par le 1er.
-                     $currentDay = 1;
-                     $calendar .= "</tr><tr>";
-                     // La variable $ dayOfWeek est utilisée pour
-                     // assurez-vous que le calendrier
-                     // affichage se compose exactement de 7 colonnes.
-                     if ($dayOfWeek > 0) {
-                         $calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>";
-                     }
-                     $month = str_pad($month, 2, "0", STR_PAD_LEFT);
-                     while ($currentDay <= $numberDays) {
+            function build_calendar($month, $year) {
+                 setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
+                // Tableau du jour de la semaine.
+                $daysOfWeek = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
+                //Quel est le premier jour du mois en question?
+                $firstDayOfMonth = mktime(0, 0, 0, $month, 1, $year);
+                // Combien de jours contient ce mois-ci?
+                $numberDays = date('t', $firstDayOfMonth);
+                // Récupérer des informations sur le premier jour de la
+                // mois en question.
+                $dateComponents = getdate($firstDayOfMonth);
+                // Quel est le nom du mois en question?
+                $monthName = $dateComponents['month'];
+                // Quelle est la valeur de l'indice (0-6) du premier jour de la
+                // mois en question.
+                $dayOfWeek = $dateComponents['wday'] - 1;
+                // Mettre Lundi en 1er
+                if ($dayOfWeek < 0) {
+                    $dayOfWeek = 6;
+                }
+                // Crée l'ouvre-étiquette de table et les en-têtes de jour
+                $calendar = "<table class='calendar'>";
+                $calendar .= "<caption>" . ucfirst(strftime('%B', $dateComponents[0])) . ' ' .  "$year</caption>";
+                $calendar .= "<tr>";
+                // Crée les en-têtes de calendrier
+                foreach ($daysOfWeek as $day) {
+                    $calendar .= "<th class='header'>$day</th>";
+                }
+                // Crée le reste du calendrier
+                // Lancer le compteur du jour, en commençant par le 1er.
+                $currentDay = 1;
+                $calendar .= "</tr><tr>";
+                // La variable $ dayOfWeek est utilisée pour
+                // assurez-vous que le calendrier
+                // affichage se compose exactement de 7 colonnes.
+                if ($dayOfWeek > 0) {
+                    $calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>";
+                }
+                $month = str_pad($month, 2, "0", STR_PAD_LEFT);
+                while ($currentDay <= $numberDays) {
 // Septième colonne (samedi) a atteint. Démarrez une nouvelle ligne.
-                         if ($dayOfWeek == 7) {
-                             $dayOfWeek = 0;
-                             $calendar .= "</tr><tr>";
-                         }
-                         $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
-                         $date = "$year-$month-$currentDayRel";
-                         $calendar .= "<td class='day' rel='$date'>$currentDay</td>";
-                         // Increment Counters
-                         $currentDay++;
-                         $dayOfWeek++;
-                     }
-                     // Complète la ligne de la dernière semaine en mois, si nécessaire
-                     if ($dayOfWeek != 7) {
-                         $remainingDays = 7 - $dayOfWeek;
-                         $calendar .= "<td colspan='$remainingDays'>&nbsp;</td>";
-                     }
-                     $calendar .= "</tr>";
-                     $calendar .= "</table>";
-                     return $calendar;
-                 }
-                 ?>
-                 <?php
-                 // Si les variables du mois et de l'année existent
-                 if (isset($_POST['months']) && isset($_POST['years'])) {
-                     // $month prend la valeur du mois
-                     $month = $_POST['months'];
-                     // $year prend la valeur de l'année
-                     $year = $_POST['years'];
-                     // On contruit le tableau avec la fonction
-                     echo build_calendar($month, $year);
-                 }
-                 ?>
-    </div>
-</body>
+                    if ($dayOfWeek == 7) {
+                        $dayOfWeek = 0;
+                        $calendar .= "</tr><tr>";
+                    }
+                    $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
+                    $date = "$year-$month-$currentDayRel";
+                    $calendar .= "<td class='day' rel='$date'>$currentDay</td>";
+                    // Increment Counters
+                    $currentDay++;
+                    $dayOfWeek++;
+                }
+                // Complète la ligne de la dernière semaine en mois, si nécessaire
+                if ($dayOfWeek != 7) {
+                    $remainingDays = 7 - $dayOfWeek;
+                    $calendar .= "<td colspan='$remainingDays'>&nbsp;</td>";
+                }
+                $calendar .= "</tr>";
+                $calendar .= "</table>";
+                return $calendar;
+            }
+            ?>
+            <?php
+            // Si les variables du mois et de l'année existent
+            if (isset($_POST['months']) && isset($_POST['years'])) {
+                // $month prend la valeur du mois
+                $month = $_POST['months'];
+                // $year prend la valeur de l'année
+                $year = $_POST['years'];
+                // On contruit le tableau avec la fonction
+                echo build_calendar($month, $year);
+                
+            }
+            ?>
+        </div>
+    </body>
 </html>
